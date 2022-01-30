479b1e1ca786ca9aa937031e999db652a41448b0
jeezy1052@gmail.com <jeezy1052@gmail.com>
2022-01-29 20:34:50 -0800

    got back end talking

--- src/components/ReviewHero.js
+++ src/components/ReviewHero.js
@@ -2,31 +2,44 @@ import React from 'react'
 
 import { FormContainer, FormInput, RadioButton, RadioButtonContainer, RadioButtonContainerInner, ReviewHeroContainer } from '../styles/ReviewHero.style'
 
+const submitHandler = (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.currentTarget)
+  const values = Object.fromEntries(formData.entries())
+
+  fetch('http://localhost:5000/posts', {
+    method: 'post',
+    body: JSON.stringify(values)
+  }).then(response => response.json()).then(console.log(values))
+}
+
 const ReviewHero = () => {
   return (
     <ReviewHeroContainer>
+        <form onSubmit={submitHandler}>
         <FormContainer>
            <h3>Leave A Review Today</h3>
-           <FormInput placeholder='UserName'/>
-           <FormInput placeholder='Strain' />
+           <FormInput name="userName" placeholder='UserName'/>
+           <FormInput name="strainName" placeholder='Strain' />
            <RadioButtonContainer>
                <RadioButtonContainerInner>
                    <RadioButton name="skunk" type="radio" />
-                   <label for="skunk" class="Skunk">Skunk</label>
-                   <RadioButton name="skunk" type="radio" />
-                   <label for="skunk" class="F">Fire</label>
-                   <RadioButton name="skunk" type="radio" />
-                   <label for="skunk" class="Skunk">Stuck</label>
-                   <RadioButton name="skunk" type="radio" />
-                   <label for="skunk" class="Da">Dank</label>
-                   <RadioButton name="skunk" type="radio" />
-                   <label for="skunk" class="Skunk">H.O.F</label>
+                   <label for="skunk">Skunk</label>
+                   <RadioButton name="fire" type="radio" />
+                   <label for="skunk">Fire</label>
+                   <RadioButton name="stuck" type="radio" />
+                   <label for="skunk">Stuck</label>
+                   <RadioButton name="dank" type="radio" />
+                   <label for="skunk">Dank</label>
+                   <RadioButton name="hof" type="radio" />
+                   <label for="skunk">H.O.F</label>
                </RadioButtonContainerInner>
            </RadioButtonContainer>
-           <FormInput className='text-area' placeholder="Leave a review" />
-           <input className="input-file" type="file" />
+           <FormInput name="review" className='text-area' placeholder="Leave a review" />
+           <input name="selectedFile" className="input-file" type="file" />
            <button className="btn-submit" type="submit">Submit</button>
         </FormContainer>
+        </form>
     </ReviewHeroContainer>
   )
 }
